53e25324b9c9e03b200674e6059274f419348504
Son Tran <son.tran@enouvo.com>
2025-07-22 22:29:16 +0700

    feat(master): update porofiolo

--- api/services/transcript_service.py
+++ api/services/transcript_service.py
@@ -2,12 +2,17 @@ import requests
 
 
 def fetch_and_process_transcript(video_id):
-    url = f"https://youtubenavigator.com/api/fetch-transcript?url=https://www.youtube.com/watch?v={video_id}&timestamps=true"
-    response = requests.get(url)
+    url = "https://tactiq-apps-prod.tactiq.io/transcript"
+    response = requests.post(
+        url,
+        headers={"Content-Type": "application/json"},
+        json={"videoUrl": f"https://www.youtube.com/watch?v={video_id}"},
+    )
     if response.status_code == 200:
         json_data = response.json()
         scripts = ""
-        for item in json_data.get("segments", {}):
-            scripts += item.get("text", "")
+        for item in json_data.get("captions", []):
+            if item.get("text") is not None and item.get("text", "") != "No text":
+                scripts += item.get("text", "")
         return scripts
-    return None
+    return "@424"
