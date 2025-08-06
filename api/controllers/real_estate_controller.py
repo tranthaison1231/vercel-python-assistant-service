03eeb16d0ba4e949238bd90687a15dc443a4055f
Son Tran <son.tran@enouvo.com>
2025-08-06 16:10:41 +0700

    feat(master): update real estate controller

--- api/controllers/real_estate_controller.py
+++ api/controllers/real_estate_controller.py
@@ -1,3 +1,4 @@
+import unicodedata
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from flask import Blueprint, jsonify
@@ -9,7 +10,19 @@ from api.services.bds68_news_service import get_bds68_news
 real_estate_bp = Blueprint("real_estate", __name__, url_prefix="/real_estate")
 
 
-ignore_titles = ["Lê Thị Riêng", "Xuân Thiều", "Trường Chinh", "Phước Lý", "sông hàn"]
+def normalize_text(text):
+    return unicodedata.normalize("NFC", text).lower()
+
+
+ignore_titles = [
+    "Lê Thị Riêng",
+    "Xuân Thiều",
+    "Trường Chinh",
+    "Phước Lý",
+    "sông hàn",
+    "Hoà Thọ",
+    "hòa hiệp",
+]
 
 
 @real_estate_bp.route("/news", methods=["GET"])
@@ -26,20 +39,23 @@ def get_real_estate_news():
             except Exception as e:
                 print(f"Error fetching news from one source: {e}")
 
-    list = [
+    ignore_titles_normalized = [normalize_text(k) for k in ignore_titles]
+
+    filtered_list = [
         item
         for item in list
         if not any(
-            keyword.lower() in item["title"].lower() for keyword in ignore_titles
+            keyword in normalize_text(item["title"])
+            for keyword in ignore_titles_normalized
         )
     ]
 
-    if list:
+    if filtered_list:
         return jsonify(
             {
                 "status": "success",
-                "count": len(list),
-                "list": list,
+                "count": len(filtered_list),
+                "list": filtered_list,
             }
         ), 200
     else:
